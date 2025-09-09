534f17f13f663a4b5fa0c835cf20d93278b19db6
alecsearle <searle.alec@gmail.com>
2025-09-09 09:58:35 -0600

    ai code for mnodel/index.js - needs verification

--- index.js
+++ index.js
@@ -1,203 +1,64 @@
 const express = require("express");
 const cors = require("cors");
 const multer = require("multer");
-const model = require("./model");
-const session = require("express-session");
+const { HubSpotClient } = require("./model");
+require("dotenv").config();
 
 const app = express();
 app.use(express.static("public"));
 app.use(cors());
 
-// MIDDLEWARES
-function authorizeUser(req, res, next) {
-  console.log("Current user session:", req.session);
-  if (req.session && req.session.userId) {
-    // user is authenticated
-    model.User.findOne({
-      _id: req.sessionn.userId,
-    }).then(function (user) {
-      if (user) {
-        req.user = user;
-        next();
-      } else {
-        // user is not authenticated: no matching user in the DB
-        res.sendStatus(401);
-      }
+// Initialize HubSpot client
+const hubspot = new HubSpotClient();
+
+// Middlewares
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+
+// File upload setup (for any documents)
+const upload = multer({
+  dest: "uploads/",
+  limits: { fileSize: 10 * 1024 * 1024 }, // 10MB limit
+});
+
+// Merchant preapproval endpoint
+app.post("/api/preapproval", upload.array("documents"), async (req, res) => {
+  try {
+    console.log("Received preapproval application:", req.body);
+
+    // Process the form data and send to HubSpot
+    const result = await hubspot.createMerchantApplication(req.body);
+
+    if (result.success) {
+      res.json({
+        success: true,
+        applicationId: result.applicationId,
+        message: "Application submitted successfully",
+      });
+    } else {
+      throw new Error(result.error);
+    }
+  } catch (error) {
+    console.error("Error processing preapproval:", error);
+    res.status(500).json({
+      success: false,
+      message: "Error submitting application. Please try again.",
     });
-  } else {
-    // user is NOT authenticated
-    res.sendStatus(401);
   }
-}
-
-// if using application/json Content-Type
-app.use(express.json());
-
-// if using application/x-www.form-urlencoded Content-Type:
-app.use(express.urlencoded({ extended: false }));
-
-app.use(
-  session({
-    secret: "jljmwurp49ut409tip4qjltrjhiegw4testrdytfug2kl34j24o5ut",
-    saveUninitialized: true,
-    resave: false,
-  })
-);
-
-// if using multipart/form-data
-// app.use(multer().none());
-
-app.get("/veggies", authorizeUser, function (req, res) {
-  model.Veggie.find({}).then((veggies) => {
-    res.json(veggies);
-  });
 });
 
-app.post("/veggies", authorizeUser, function (req, res) {
-  console.log("Parsed request body: ", req.body);
-
-  let newVeggie = new model.Veggie({
-    name: req.body.name,
-    color: req.body.color,
-    rating: req.body.rating,
-    user: req.user._id,
-  });
-  newVeggie
-    .save()
-    .then(() => {
-      // The veggie was saved to the DB successfully!
-      res.status(201).send("Created");
-    })
-    .catch((error) => {
-      // validate client inputs
-      if (error.errors) {
-        // There are mongoose validation errors
-        // make a simple JSON object to return to the client
-        let errorMessages = {};
-        for (let field in error.errors) {
-          errorMessages[field] = error.errors[field].message;
-        }
-        res.status(422).json(errorMessages);
-      } else if (error.code == 11000) {
-        res.status(422).json({
-          // database uniqueness violation
-          email: "User with email already exists",
-        });
-      } else {
-        // something bad happened...
-        console.error("Failed to save veggie to DB", error);
-        res.sendStatus(500);
-      }
-    });
-
-  res.status(201).send("Created");
+// Health check endpoint
+app.get("/api/health", (req, res) => {
+  res.json({ status: "Server is running" });
 });
 
-app.delete(`/veggies/:veggieId`, function (req, res) {
-  // does the veggie even exist???
-  model.Veggie.findOne({ _id: req.params.veggieId }).then(function (veggie) {
-    // if it does exist:
-    if (veggie) {
-      // delete the veggie (mongoose things)
-      model.Veggie.deleteOne({
-        _id: req.params.veggieId,
-      }).then(function () {
-        // respond to the client: 200 OK or 204 No Content
-        res.sendStatus(200);
-      });
-    } else {
-      // respond to the client: 404 Not Found
-      res.sendStatus(200);
-    }
-  });
+// Serve the main form
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
 });
 
-// endpoint: create a user (i.e. register)
-app.post(`/users`, function (req, res) {
-  console.log("Parsed request body: ", req.body);
-
-  let newUser = new model.User({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-  });
-
-  // password
-  newUser
-    .setEncryptedPassword(req.body.plainPassword)
-    .then(function () {
-      // promise has been fulfilled
-      newUser
-        .save()
-        .then(() => {
-          // the user was saved to the DB successfully
-          res.status(201).send("Created");
-        })
-        .catch((error) => {
-          // validate client inputs
-          if (error.errors) {
-            // There are mongoose validation errors
-            // make a simple JSON object to return to the client
-            let errorMessages = {};
-            for (let field in error.errors) {
-              errorMessages[field] = error.errors[field].message;
-            }
-            res.status(422).json(errorMessages);
-          } else if (error.code == 11000) {
-            res.status(422).json({
-              // database uniqueness violation
-              email: "User with email already exists",
-            });
-          } else {
-            // something bad happened...
-            console.error("Failed to save User to DB", error);
-            res.sendStatus(500);
-          }
-          // validate client inputs
-          console.error("Failed to save user to DB", error);
-        });
-      res.status(201).send("Created");
-    })
-    .catch(function () {
-      // promise could not be fulfilled
-    });
-});
-
-app.get("/session", authorizeUser, function (req, res) {
-  req.json(req.user);
-});
-
-// endpoint: authenticate a user (in a session)
-app.post("/session", function (req, res) {
-  // check if the user exists )(by email)
-  model.User.findOne({
-    email: req.body.email,
-  }).then(function (user) {
-    if (user) {
-      // if user exists,  verify hashed password in DB matches given pw
-      user.verifyEncryptedPassword(req.body.plainPassword).then(function (verified) {
-        if (verified) {
-          // if verified, record user into the session (authenticated)
-          // TODO: save user to a session object!!!
-          req.session.userId = user._id;
-          res.sendStatus(201);
-        } else {
-          // if not verified, return status 401
-          res.sendStatus(401);
-        }
-      });
-    } else {
-      // if user does not exist, return status 401
-      res.sendStatus(401);
-    }
-  });
-});
-
-// endpoint: un-authenticate a user (log out)
-app.delete("/session", authorizeUser, function (req, res) {
-  req.session.userId = null;
-  res.sendStatus(200);
-});
+// Keep your existing user/session endpoints if needed
+// ... (your existing user/session code)
 
 app.listen(8080, function () {
   console.log("Server ready. Listening on port 8080");
--- model.js
+++ model.js
@@ -1,81 +1,287 @@
-// const bcrypt = require("bcrypt");
-// const mongoose = require("mongoose");
+const axios = require("axios");
 
-// mongoose.connect("mongodb+srv://alecsearle:ixEzRzLa3UiF3dsI@apexapp.kljz2.mongodb.net/?retryWrites=true&w=majority&appName=ApexApp", {
-//   dbName: "fridge",
-// });
+const HUBSPOT_API_KEY = process.env.HUBSPOT_API_KEY;
+const HUBSPOT_BASE_URL = "https://api.hubapi.com";
 
-// const Veggie = mongoose.model("Veggie", {
-//   name: {
-//     type: String,
-//     required: true,
-//   },
-//   color: {
-//     type: String,
-//     required: true,
-//   },
-//   rating: {
-//     type: Number,
-//     required: true,
-//     min: 1,
-//     max: 10,
-//   },
-//   good: {
-//     type: Boolean,
-//     required: true,
-//   },
-//   createdAt: {
-//     type: Date,
-//   },
-//   user: {
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: "User",
-//   },
-// });
+class HubSpotClient {
+  constructor() {
+    this.apiKey = HUBSPOT_API_KEY;
+    this.baseUrl = HUBSPOT_BASE_URL;
+  }
 
-// const userSchema = new mongoose.Schema({
-//   email: {
-//     type: String,
-//     unique: true,
-//   },
-//   encryptedPassword: {
-//     type: String,
-//   },
-//   firstName: {
-//     type: String,
-//   },
-//   lastName: {
-//     type: String,
-//   },
-// });
+  // Create a new contact (merchant) in HubSpot
+  async createContact(contactData) {
+    try {
+      const url = `${this.baseUrl}/crm/v3/objects/contacts`;
 
-// userSchema.methods.setEncryptedPassword = function (plainPassword) {
-//   let promise = new Promise((resolve, reject) => {
-//     // this is the promise function
-//     // resolve and reject are also functions.
-//     bcrypt.hash(plainPassword, 12).then((hash) => {
-//       console.log(`hashed pw: `, hash);
-//       this.encryptedPassword = hash;
-//       resolve();
-//     });
-//   });
-//   // return the promise for future
-//   return promise;
-// };
+      const hubspotContact = {
+        properties: {
+          email: contactData.businessEmail,
+          firstname: contactData.ownerFirstName,
+          lastname: contactData.ownerLastName,
+          company: contactData.dbaName,
+          phone: contactData.customerSupportPhone,
+          website: contactData.businessWebsite,
+          address: contactData.physicalAddress,
+          city: contactData.city,
+          state: contactData.state,
+          zip: contactData.zipCode,
 
-// userSchema.methods.verifyEncryptedPassword = function (plainPassword) {
-//   let promise = new Promise((resolve, reject) => {
-//     bcrypt.compare(plainPassword, this.encryptedPassword).then((result) => {
-//       resolve(result);
-//     });
-//   });
-//   // return the promise for future
-//   return promise;
-// };
+          // Custom properties for merchant-specific data
+          merchant_product_service: contactData.productService,
+          merchant_monthly_volume: contactData.monthlyVolume,
+          merchant_avg_ticket: contactData.avgTicket,
+          merchant_years_in_business: contactData.yearsInBusiness,
+          merchant_entity_type: contactData.entityType,
+          merchant_gateway: contactData.gateway,
+          merchant_fulfillment_center: contactData.fulfillmentCenter,
+          merchant_industry_type: contactData.productService,
+          merchant_processing_type: this.formatProcessingTypes(contactData),
+          merchant_ownership_percentage: contactData.ownershipPercent,
+          merchant_ssn: contactData.ownerSSN, // Be careful with PII - consider encryption
+          merchant_drivers_license: contactData.ownerDL,
+        },
+      };
 
-// const User = mongoose.model("User", userSchema);
+      const response = await axios.post(url, hubspotContact, {
+        headers: {
+          Authorization: `Bearer ${this.apiKey}`,
+          "Content-Type": "application/json",
+        },
+      });
 
-// module.exports = {
-//   User,
-//   Veggie, //shorthand for Veggie: Veggie
-// };
+      return response.data;
+    } catch (error) {
+      console.error("Error creating HubSpot contact:", error.response?.data || error.message);
+      throw error;
+    }
+  }
+
+  // Create a deal (merchant application) in HubSpot
+  async createDeal(dealData, contactId) {
+    try {
+      const url = `${this.baseUrl}/crm/v3/objects/deals`;
+
+      const hubspotDeal = {
+        properties: {
+          dealname: `Merchant Application - ${dealData.dbaName}`,
+          amount: dealData.monthlyVolume,
+          dealstage: "qualifiedtobuy", // Adjust based on your pipeline
+          pipeline: "default", // Use your HubSpot pipeline ID
+          closedate: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000).toISOString(), // 30 days from now
+
+          // Custom deal properties
+          merchant_application_id: `APP-${Date.now()}`,
+          merchant_risk_level: this.assessRiskLevel(dealData.productService),
+          merchant_gateway_preference: dealData.gateway,
+          merchant_monthly_volume: dealData.monthlyVolume,
+          merchant_high_ticket: dealData.highTicket,
+          merchant_avg_ticket: dealData.avgTicket,
+          merchant_amex_percentage: dealData.expectedAmex,
+          merchant_business_percentage: dealData.businessPercent,
+          merchant_consumer_percentage: dealData.consumerPercent,
+
+          // Processing breakdown
+          merchant_swiped_percent: dealData.swipedPercent,
+          merchant_keyed_percent: dealData.keyedPercent,
+          merchant_moto_percent: dealData.motoPercent,
+          merchant_ecomm_percent: dealData.ecommPercent,
+
+          // Banking info (be careful with sensitive data)
+          merchant_bank_name: dealData.bankName,
+          merchant_routing_number: dealData.routingNumber,
+
+          // Additional details
+          merchant_owns_inventory: dealData.ownInventory,
+          merchant_storage_location: dealData.storedAtBusiness,
+          merchant_shipment_days: dealData.shipmentDays,
+          merchant_ssl_encryption: dealData.sslEncryption,
+        },
+        associations: [
+          {
+            to: { id: contactId },
+            types: [
+              {
+                associationCategory: "HUBSPOT_DEFINED",
+                associationTypeId: 3, // Deal to Contact association
+              },
+            ],
+          },
+        ],
+      };
+
+      const response = await axios.post(url, hubspotDeal, {
+        headers: {
+          Authorization: `Bearer ${this.apiKey}`,
+          "Content-Type": "application/json",
+        },
+      });
+
+      return response.data;
+    } catch (error) {
+      console.error("Error creating HubSpot deal:", error.response?.data || error.message);
+      throw error;
+    }
+  }
+
+  // Create a note with all application details
+  async createNote(noteData, contactId, dealId) {
+    try {
+      const url = `${this.baseUrl}/crm/v3/objects/notes`;
+
+      const noteBody = this.formatApplicationNote(noteData);
+
+      const hubspotNote = {
+        properties: {
+          hs_note_body: noteBody,
+          hs_attachment_ids: "", // Add if you have file uploads
+        },
+        associations: [
+          {
+            to: { id: contactId },
+            types: [
+              {
+                associationCategory: "HUBSPOT_DEFINED",
+                associationTypeId: 190, // Note to Contact
+              },
+            ],
+          },
+          {
+            to: { id: dealId },
+            types: [
+              {
+                associationCategory: "HUBSPOT_DEFINED",
+                associationTypeId: 214, // Note to Deal
+              },
+            ],
+          },
+        ],
+      };
+
+      const response = await axios.post(url, hubspotNote, {
+        headers: {
+          Authorization: `Bearer ${this.apiKey}`,
+          "Content-Type": "application/json",
+        },
+      });
+
+      return response.data;
+    } catch (error) {
+      console.error("Error creating HubSpot note:", error.response?.data || error.message);
+      throw error;
+    }
+  }
+
+  // Helper function to assess risk level based on industry
+  assessRiskLevel(productService) {
+    const highRiskIndustries = [
+      "adult-entertainment",
+      "cbd-hemp",
+      "cryptocurrency",
+      "gambling",
+      "firearms",
+      "online-pharmacy",
+    ];
+
+    const mediumRiskIndustries = ["e-cigarettes", "travel", "nutraceuticals", "forex-trading"];
+
+    if (highRiskIndustries.includes(productService)) {
+      return "High Risk";
+    } else if (mediumRiskIndustries.includes(productService)) {
+      return "Medium Risk";
+    } else {
+      return "Low Risk";
+    }
+  }
+
+  // Format processing types for display
+  formatProcessingTypes(data) {
+    const types = [];
+    if (parseInt(data.swipedPercent) > 0) types.push(`Swiped: ${data.swipedPercent}%`);
+    if (parseInt(data.keyedPercent) > 0) types.push(`Keyed: ${data.keyedPercent}%`);
+    if (parseInt(data.motoPercent) > 0) types.push(`MOTO: ${data.motoPercent}%`);
+    if (parseInt(data.ecommPercent) > 0) types.push(`Ecommerce: ${data.ecommPercent}%`);
+    return types.join(", ");
+  }
+
+  // Create detailed note body
+  formatApplicationNote(data) {
+    return `
+<h3>Merchant Preapproval Application</h3>
+<p><strong>Application ID:</strong> APP-${Date.now()}</p>
+<p><strong>Submitted:</strong> ${new Date().toLocaleString()}</p>
+
+<h4>Business Information</h4>
+<ul>
+  <li><strong>DBA Name:</strong> ${data.dbaName}</li>
+  <li><strong>Legal Name:</strong> ${data.legalName}</li>
+  <li><strong>Product/Service:</strong> ${data.productService}</li>
+  <li><strong>Years in Business:</strong> ${data.yearsInBusiness}</li>
+  <li><strong>Entity Type:</strong> ${data.entityType}</li>
+  <li><strong>Website:</strong> ${data.businessWebsite || "N/A"}</li>
+</ul>
+
+<h4>Processing Details</h4>
+<ul>
+  <li><strong>Monthly Volume:</strong> $${data.monthlyVolume}</li>
+  <li><strong>Average Ticket:</strong> $${data.avgTicket}</li>
+  <li><strong>High Ticket:</strong> $${data.highTicket}</li>
+  <li><strong>Gateway Preference:</strong> ${data.gateway}</li>
+  <li><strong>Processing Mix:</strong> Swiped ${data.swipedPercent}%, Keyed ${
+      data.keyedPercent
+    }%, MOTO ${data.motoPercent}%, Ecommerce ${data.ecommPercent}%</li>
+</ul>
+
+<h4>Banking Information</h4>
+<ul>
+  <li><strong>Bank Name:</strong> ${data.bankName}</li>
+  <li><strong>Routing Number:</strong> ${data.routingNumber}</li>
+</ul>
+
+<h4>Additional Notes</h4>
+<ul>
+  <li><strong>Fulfillment Center:</strong> ${data.fulfillmentCenter}</li>
+  <li><strong>Owns Inventory:</strong> ${data.ownInventory}</li>
+  <li><strong>SSL Encryption:</strong> ${data.sslEncryption}</li>
+  <li><strong>Shipment Timeline:</strong> ${data.shipmentDays} days</li>
+</ul>
+    `.trim();
+  }
+
+  // Main function to create complete merchant application in HubSpot
+  async createMerchantApplication(applicationData) {
+    try {
+      console.log("Creating merchant application in HubSpot...");
+
+      // Step 1: Create contact
+      const contact = await this.createContact(applicationData);
+      console.log("Contact created:", contact.id);
+
+      // Step 2: Create deal
+      const deal = await this.createDeal(applicationData, contact.id);
+      console.log("Deal created:", deal.id);
+
+      // Step 3: Create detailed note
+      const note = await this.createNote(applicationData, contact.id, deal.id);
+      console.log("Note created:", note.id);
+
+      return {
+        success: true,
+        contactId: contact.id,
+        dealId: deal.id,
+        noteId: note.id,
+        applicationId: `APP-${Date.now()}`,
+      };
+    } catch (error) {
+      console.error("Error creating merchant application:", error);
+      return {
+        success: false,
+        error: error.message,
+      };
+    }
+  }
+}
+
+module.exports = {
+  HubSpotClient,
+};
